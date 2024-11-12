9e471e896eb1edd52bea0b0a9cb34742393a708e
Kevin Trinidad <ktrinidad209@gmail.com>
2024-11-12 00:45:14 +0000

    Updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Get real-time info for numerous variables.
 let valueSearch = document.getElementById('valueSearch');
-let city = document.getElementById('city')
+let city = document.getElementById('city');
 let temperature = document.getElementById('temperature');
 let description = document.querySelector('.description');
 let cloud = document.getElementById('clouds');
@@ -13,13 +13,19 @@ form.addEventListener('submit', (event) => {
         searchWeather();
     }
 })
-let id = "f4431a8be9b9bfe5d1a0c35da9953709"
-// let url = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q='
-let url = 'https://api.openweathermap.org/data/3.0/weather?units=metric&appid='+id;
+let id = "f4431a8be9b9bfe5d1a0c35da9953709";
+let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid='+id;
 const searchWeather = () => {
-    fetch(url)
+    fetch(url + '&q=' + valueSearch.value)
     .then(responsive => responsive.json())
     .then(data => {
         console.log(data);
+        if(data.cod == 200) {
+            city.querySelector('figcaption').innerText = data.name;
+            city.querySelector('img').src="https://flagsapi.com/"+data.sys.country+"/shiny/32.png"
+
+            temperature.querySelector('img').src="https://openweathermap.org/img/" + data.weather[0].icon+ "@4x.png";
+            temperature.querySelector('figcaption span').innerText = data.main.temp;
+        }
     })
 }
