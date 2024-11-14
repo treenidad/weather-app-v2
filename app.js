e6db9f91c2eb2da611fadbc6b99b81ad92a88cbf
Kevin Trinidad <ktrinidad209@gmail.com>
2024-11-14 00:54:45 +0000

    Updated

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let cloud = document.getElementById('clouds');
 let humidity = document.getElementById('humidity');
 let pressure = document.getElementById('pressure');
 let form = document.querySelector('form');
+let main = document.querySelector('main');
 form.addEventListener('submit', (event) => {
     event.preventDefault(); // Prevents automatic refresh of website
     if (valueSearch.value != ''){
@@ -14,7 +15,7 @@ form.addEventListener('submit', (event) => {
     }
 })
 let id = "f4431a8be9b9bfe5d1a0c35da9953709";
-let url = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid='+id;
+let url = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid='+id;
 const searchWeather = () => {
     fetch(url + '&q=' + valueSearch.value)
     .then(responsive => responsive.json())
@@ -26,6 +27,23 @@ const searchWeather = () => {
 
             temperature.querySelector('img').src="https://openweathermap.org/img/" + data.weather[0].icon+ "@4x.png";
             temperature.querySelector('figcaption span').innerText = data.main.temp;
+            description.innerText = data.weather[0].description;
+            clouds.innerText = data.clouds.all;
+            humidity.innerText = data.main.humidity;
+            pressure.innerText = data.main.pressure;
+        } else {
+            // false
+            main.classList.add('error'); 
+            setTimeout(() => {
+                main.classList.remove('error');
+            }, 1000);
         }
+
+        valueSearch.value = '';
     })
 }
+const initApp = () => {
+    valueSearch.value = 'California';
+    searchWeather();
+}
+initApp();
