0e5131d8f15a8749c1bca9eb4f0f3f6c3ec02d85
Kevin Trinidad <ktrinidad209@gmail.com>
2024-11-15 09:20:30 +0000

    Updated

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ let temperature = document.getElementById('temperature');
 let description = document.querySelector('.description');
 let cloud = document.getElementById('clouds');
 let humidity = document.getElementById('humidity');
-let pressure = document.getElementById('pressure');
+// let pressure = document.getElementById('pressure');
+let wind = document.getElementById('wind')
 let form = document.querySelector('form');
 let main = document.querySelector('main');
+
 form.addEventListener('submit', (event) => {
     event.preventDefault(); // Prevents automatic refresh of website
     if (valueSearch.value != ''){
@@ -23,14 +25,34 @@ const searchWeather = () => {
         console.log(data);
         if(data.cod == 200) {
             city.querySelector('figcaption').innerText = data.name;
-            city.querySelector('img').src="https://flagsapi.com/"+data.sys.country+"/shiny/32.png"
+            // city.querySelector('img').src="https://flagsapi.com/"+data.sys.country+"/shiny/32.png"
 
-            temperature.querySelector('img').src="https://openweathermap.org/img/" + data.weather[0].icon+ "@4x.png";
-            temperature.querySelector('figcaption span').innerText = data.main.temp;
-            description.innerText = data.weather[0].description;
+            temperature.querySelector('img').src="https://openweathermap.org/img/wn/" + data.weather[0].icon + "@4x.png";
+            temperature.querySelector('figcaption span').innerText = Math.round(data.main.temp) + "°F";
+            description.innerText = capitalizeWords(data.weather[0].description);
             clouds.innerText = data.clouds.all;
             humidity.innerText = data.main.humidity;
-            pressure.innerText = data.main.pressure;
+            wind.innerText = Math.round(data.wind.speed);
+            // pressure.innerText = data.main.pressure;
+
+            // Determine the appropriate weather icon to show.
+            if (data.weather[0].main == "Clouds") {
+                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/02d@4x.png";
+            } else if (data.weather[0].main == "Clear") {
+                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/01d@4x.png";
+            } else if (data.weather[0].main == "Rain") {
+                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/10d@4x.png";
+            } else if (data.weather[0].main == "Drizzle") {
+                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/09d@4x.png";
+            } else if (data.weather[0].main == "Mist") {
+                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/50d@4x.png";
+            } else if (data.weather[0].main == "Snow") {
+                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/13d@4x.png";
+            }
+
+            // document.querySelector("main").style.display = "none";
+
+
         } else {
             // false
             main.classList.add('error'); 
@@ -38,12 +60,19 @@ const searchWeather = () => {
                 main.classList.remove('error');
             }, 1000);
         }
-
         valueSearch.value = '';
     })
+
 }
+
+function capitalizeWords(str) {
+    return str.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+}
+
 const initApp = () => {
-    valueSearch.value = 'California';
+    valueSearch.value = 'Stockton';
     searchWeather();
 }
 initApp();
