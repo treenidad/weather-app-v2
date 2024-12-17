77b65a60cfbd52c7ebbbcbc15901dc279580e6cf
treenidad <ktrinidad209@gmail.com>
2024-12-16 21:51:11 -0800

    Updated README file and improved weather icon display.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # weather-app-v2
-An updated version of the weather-app repository
+
+A simple weather app that shows the current weather for any city using the OpenWeatherMap API. This project was built as part of a YouTube tutorial by [Lun Dev Code](https://www.youtube.com/watch?v=WlYknHl_8fM&t=3s).
+
+## Features
+
+- Search for weather by city name
+- Displays temperature, humidity, wind, and weather description
+
+## Acknowledgment
+
+This project was created by following the tutorial **"Weather App Easy with Javascript | Code Free"** by [Lun Dev Code](https://www.youtube.com/watch?v=WlYknHl_8fM&t=3s).
+
+## Tech Stack
+
+- HTML, CSS, JavaScript
+- OpenWeatherMap API
+
+## Changes from the original
+
+- Changed the color of the card to a linear-gradient.
+- Replaced "Pressure" data with "Wind" data.
+- Adjusted the code to better match the correct weather image for day/night.
--- app.js
+++ app.js
@@ -1,78 +1,90 @@
 // Get real-time info for numerous variables.
-let valueSearch = document.getElementById('valueSearch');
-let city = document.getElementById('city');
-let temperature = document.getElementById('temperature');
-let description = document.querySelector('.description');
-let cloud = document.getElementById('clouds');
-let humidity = document.getElementById('humidity');
+let valueSearch = document.getElementById("valueSearch");
+let city = document.getElementById("city");
+let temperature = document.getElementById("temperature");
+let description = document.querySelector(".description");
+let cloud = document.getElementById("clouds");
+let humidity = document.getElementById("humidity");
 // let pressure = document.getElementById('pressure');
-let wind = document.getElementById('wind')
-let form = document.querySelector('form');
-let main = document.querySelector('main');
+let wind = document.getElementById("wind");
+let form = document.querySelector("form");
+let main = document.querySelector("main");
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Prevents automatic refresh of website
-    if (valueSearch.value != ''){
-        searchWeather();
-    }
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Prevents automatic refresh of website
+  if (valueSearch.value != "") {
+    searchWeather();
+  }
+});
 let id = "f4431a8be9b9bfe5d1a0c35da9953709";
-let url = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid='+id;
+let url =
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=" + id;
 const searchWeather = () => {
-    fetch(url + '&q=' + valueSearch.value)
-    .then(responsive => responsive.json())
-    .then(data => {
-        console.log(data);
-        if(data.cod == 200) {
-            city.querySelector('figcaption').innerText = data.name;
-            // city.querySelector('img').src="https://flagsapi.com/"+data.sys.country+"/shiny/32.png"
+  fetch(url + "&q=" + valueSearch.value)
+    .then((responsive) => responsive.json())
+    .then((data) => {
+      console.log(data);
+      if (data.cod == 200) {
+        city.querySelector("figcaption").innerText = data.name;
+        // city.querySelector('img').src="https://flagsapi.com/"+data.sys.country+"/shiny/32.png"
 
-            temperature.querySelector('img').src="https://openweathermap.org/img/wn/" + data.weather[0].icon + "@4x.png";
-            temperature.querySelector('figcaption span').innerText = Math.round(data.main.temp) + "°F";
-            description.innerText = capitalizeWords(data.weather[0].description);
-            clouds.innerText = data.clouds.all;
-            humidity.innerText = data.main.humidity;
-            wind.innerText = Math.round(data.wind.speed);
-            // pressure.innerText = data.main.pressure;
+        temperature.querySelector("img").src =
+          "https://openweathermap.org/img/wn/" +
+          data.weather[0].icon +
+          "@4x.png";
+        temperature.querySelector("figcaption span").innerText =
+          Math.round(data.main.temp) + "°F";
+        description.innerText = capitalizeWords(data.weather[0].description);
+        clouds.innerText = data.clouds.all;
+        humidity.innerText = data.main.humidity;
+        wind.innerText = Math.round(data.wind.speed);
+        // pressure.innerText = data.main.pressure;
 
-            // Determine the appropriate weather icon to show.
-            if (data.weather[0].main == "Clouds") {
-                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/02d@4x.png";
-            } else if (data.weather[0].main == "Clear") {
-                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/01d@4x.png";
-            } else if (data.weather[0].main == "Rain") {
-                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/10d@4x.png";
-            } else if (data.weather[0].main == "Drizzle") {
-                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/09d@4x.png";
-            } else if (data.weather[0].main == "Mist") {
-                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/50d@4x.png";
-            } else if (data.weather[0].main == "Snow") {
-                temperature.querySelector('img').src = "https://openweathermap.org/img/wn/13d@4x.png";
-            }
+        // Determine the appropriate weather icon to show.
+        // if (data.weather[0].main == "Clouds") {
+        //   if (data.weather[0].icon == "01d")
+        //     temperature.querySelector("img").src =
+        //       "https://openweathermap.org/img/wn/02d@4x.png";
+        // } else if (data.weather[0].main == "Clear") {
+        //   temperature.querySelector("img").src =
+        //     "https://openweathermap.org/img/wn/01d@4x.png";
+        // } else if (data.weather[0].main == "Rain") {
+        //   temperature.querySelector("img").src =
+        //     "https://openweathermap.org/img/wn/10d@4x.png";
+        // } else if (data.weather[0].main == "Drizzle") {
+        //   temperature.querySelector("img").src =
+        //     "https://openweathermap.org/img/wn/09d@4x.png";
+        // } else if (data.weather[0].main == "Mist") {
+        //   temperature.querySelector("img").src =
+        //     "https://openweathermap.org/img/wn/50d@4x.png";
+        // } else if (data.weather[0].main == "Snow") {
+        //   temperature.querySelector("img").src =
+        //     "https://openweathermap.org/img/wn/13d@4x.png";
+        // }
 
-            // document.querySelector("main").style.display = "none";
-
-
-        } else {
-            // false
-            main.classList.add('error'); 
-            setTimeout(() => {
-                main.classList.remove('error');
-            }, 1000);
-        }
-        valueSearch.value = '';
-    })
-
-}
+        // document.querySelector("main").style.display = "none";
+      } else {
+        // false
+        main.classList.add("error");
+        setTimeout(() => {
+          main.classList.remove("error");
+        }, 1000);
+      }
+      valueSearch.value = "";
+    });
+};
 
 function capitalizeWords(str) {
-    return str.split(' ').map(word => {
+  return str
+    .split(" ")
+    .map((word) => {
       return word.charAt(0).toUpperCase() + word.slice(1);
-    }).join(' ');
+    })
+    .join(" ");
 }
 
 const initApp = () => {
-    valueSearch.value = 'Stockton';
-    searchWeather();
-}
+  valueSearch.value = "Stockton";
+  searchWeather();
+};
 initApp();
